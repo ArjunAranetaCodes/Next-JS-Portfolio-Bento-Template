f2ea4abd2be1806387eb06ea92900a48acf8720f
MoreCodes <arjunaraneta@gmail.com>
2024-04-24 18:34:20 +1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //basePath: "/design",
-  basePath: "/design",
-  output: "export",
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
